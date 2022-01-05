51fe95d2fbce44070f57d61577e4997b9129f9e2
NiNoX14 <hardstuckd5lul@gmail.com>
2022-01-05 19:56:32 +0200

    feat: configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,17 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '/src/db/migrations/*.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
