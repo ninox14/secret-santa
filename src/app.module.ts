a289c721e5275190c402da8bd61a4750aa952f7d
NiNoX14 <hardstuckd5lul@gmail.com>
2022-01-09 18:12:25 +0200

    feat: import created modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(config), UsersModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
