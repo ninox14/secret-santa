701136590acaeb8249e41d369c01db425c84946d
NiNoX14 <hardstuckd5lul@gmail.com>
2022-01-09 18:12:43 +0200

    fix: dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Nest secret santa api')
@@ -15,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('/', app, document);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
